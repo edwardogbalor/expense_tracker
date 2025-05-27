7955f9220c7515aea4b6fde3735d36fecfc4faf2
Edward Ogbalor <edwardogbalor2@gmail.com>
2025-05-27 12:57:27 -0700

    correctly tested servers

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -3,8 +3,11 @@ import {users} from '../dummyData/data.js'
 const userResolver = {
     Query: {
         users: () => {
-            return users
-        }
+            return users;
+        },
+        user: (_, {userId}) =>{
+            return users.find((user) => user._id === userId);
+        },
     },
     Mutation: {}
 }
