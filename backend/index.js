7a07b9f05ea47f708cfc479cbef4f5feab0e8513
Edward Ogbalor <edwardogbalor2@gmail.com>
2025-05-27 13:59:20 -0700

    first part fo middleware

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+
 import mergedResolvers from './resolvers/index.js';
 import mergedTypeDefs from './typeDefs/index.js';
 
