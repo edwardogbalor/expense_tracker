ffdeb0475a9f4c11c5c4842aa6415b6a9e339962
Edward Ogbalor <edwardogbalor2@gmail.com>
2025-05-27 20:09:21 -0700

    set up databse

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,12 @@ import express from "express";
 import http from "http";
 import cors from "cors";
 
+import dotenv from 'dotenv';
 import mergedResolvers from './resolvers/index.js';
 import mergedTypeDefs from './typeDefs/index.js';
+import { connectDB } from "./db/connectDB.js";
 
+dotenv.config();
 const app = express();
 
 const httpServer = http.createServer(app);
@@ -37,5 +40,6 @@ app.use(
 
 // Modified server startup
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+await connectDB();
 
 console.log(`🚀 Server ready at http://localhost:4000/graphql`);
