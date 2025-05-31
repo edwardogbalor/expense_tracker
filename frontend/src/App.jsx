4ebe1bccb924c7af2d8efb29da3a5db95ab17180
Edward Ogbalor <edwardogbalor2@gmail.com>
2025-05-31 11:44:49 -0700

    made utils

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,16 @@
 function App() {
   return (
     <>
-      <h1 className='text-red-500 text-5xl'>Hello</h1>
+      <Routes>
+				{/*<Route path='/' element={data.authUser ? <HomePage /> : <Navigate to='/login' />} />
+				<Route path='/login' element={!data.authUser ? <LoginPage /> : <Navigate to='/' />} />*/}
+				<Route path='/signup' element={!data.authUser ? <SignUpPage /> : <Navigate to='/' />} />
+				{/*<Route
+					path='/transaction/:id'
+					element={data.authUser ? <TransactionPage /> : <Navigate to='/login' />}
+				/>
+				<Route path='*' element={<NotFoundPage />} />*/}
+			</Routes>
     </>
   )
 }
